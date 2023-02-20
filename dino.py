42994eabaf6d3989b46a6590866c8efc1fc2ae8c
= <=>
2023-02-20 09:17:32 +0530

    works

--- dino.py
+++ dino.py
@@ -1,34 +1,41 @@
 import torch
-from torchvision.models.efficientnet import efficientnet_v2_m
-import pyautogui
+from torchvision.models.efficientnet import efficientnet_v2_s
 import keyboard
-import cv2
 from PIL import Image, ImageGrab
 import numpy as np
-from torchvision.transforms import Compose, Resize, CenterCrop, ToTensor
+from torchvision.transforms import Compose, Resize, CenterCrop, ToTensor, Normalize
+from tqdm import tqdm
 
-model = efficientnet_v2_m()
+model = efficientnet_v2_s()
 model.classifier = torch.nn.Linear(in_features = 1280, out_features = 2)
-model.load_state_dict(torch.load("efficientnet.pth", map_location=torch.device('cpu')))
+model.load_state_dict(torch.load("models/efficientnet_s.pth"))
+model.to("cuda")
 model.eval()
 
+
 transformer = Compose([
-    Resize(480),
+    Resize([480,480]),
     CenterCrop(480),
-    ToTensor()
+    Normalize(mean =[0.485, 0.456, 0.406], std =[0.229, 0.224, 0.225])
 ])
 
-while(not keyboard.is_pressed("esc")):
-    image = pyautogui.screenshot()
-    image = cv2.cvtColor(np.array(ImageGrab.grab(bbox = (500,230,1400,450))), cv2.COLOR_RGB2BGR)
-    image = cv2.resize(image, dsize = (480,480))
-    image = Image.fromarray(image)
+convert_tensor = ToTensor()
+
+def generator():
+    while(not keyboard.is_pressed("esc")):
+      yield
+
+for _ in tqdm(generator()):
+    image = ImageGrab.grab(bbox = (620,220,1280,360)) 
+    image = convert_tensor(image)
+    image = image.to("cuda")
     image = transformer(image)
-    image = image.reshape(1,3,480,480)
-    outputs = model(image)
+    outputs = model(image[None , ...])
     _,preds = torch.max(outputs.data, 1)
-    print(preds.item())
-    if preds == 1:
-        keyboard.press_and_release()
+    if preds.item() == 1:
+        keyboard.press_and_release("space")
+
+
+
 
 
